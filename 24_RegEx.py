7f7dc75209ff3da09d31c800b8684220476369fc
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-07-05 13:17:56 +0530

    Day 05

--- 24_RegEx.py
+++ 24_RegEx.py
@@ -58,7 +58,7 @@ def UsingModules():
     ## FullMatch ###################################
     # Similar to match(), attempts to match from the beginning of the string, but it also has to match the entire string.
     print(len(string1))
-    pt1 = r".{284}"         # '.' matches any character, apart from a newline
+    pt1 = r".{285}"         # '.' matches any character, apart from a newline
     res = re.fullmatch(pt1, string1)
     print(res)
 
@@ -89,9 +89,46 @@ def UsingModules():
     print(res[1], "-->", res[0])
 
 def usingGroups():
-    pass
+    res = re.search(r".+\s(.+ex).+(\d\d\s.+).", string1)
+    print(res.groups())     # Lists out all the matched groups
+
+    # Individual group
+    print(res.group(1))
+    print(res.group(2))
+    print(res.group(0))
+    print(res.group())      # same as group(0)
+
+    print(res.group(1, 2))
+    
+    print(res.span(1))
+    print(res.span(2))
+
+    print(res.start(1), res.end(1))
+    print(res.start(2), res.end(2))
+
+def usingFlags():
+    res = re.findall(r'the', string1, re.I)     # Ignorecase
+    print(len(res), "-->", res)
+
+    string2 = "The Euro STOXX 600 index, which tracks all stock markets across Europe including the FTSE, fell by 11.48% - \nthe worst day since it launched in 1998. \nThe panic selling prompted by the coronavirus has wiped £2.7tn off \nthe value of STOXX 600 shares since its all-time peak on 19 February."
+    res = re.findall(r'^the', string2, re.I|re.M)       # Multiline
+    print(len(res), "-->", res)
+
+    print(len(string2))
+    pt1 = r".{288}"         # '.' matches any character, apart from a newline
+    res = re.fullmatch(pt1, string2, re.S)      # DOTALL - enable the '.' to match newline chars as well.
+    print(res)
+
+    res = re.search(r'''.+\s        # Some chars of no interest in the beginning
+                    (.+ex)          # word ending with the substring 'ex'
+                    .+              # all chars in the middle
+                    (\d\d\s.+)      # the date data we're looking for
+                    .               # the last insignificant character
+                    ''', string1, re.X)
+    print(res.groups())
 
 
 if __name__ == '__main__':
     # UsingModules()
-    usingGroups()
+    # usingGroups()
+    usingFlags()
